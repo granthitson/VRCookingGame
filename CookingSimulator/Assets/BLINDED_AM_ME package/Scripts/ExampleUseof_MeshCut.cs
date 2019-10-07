78597668669f50067bbf3b5972a63e3a6138c8ae
Grant Hitson <gkh9113@uncw.edu>
2019-10-06 21:52:03 -0400

    mesh cutting and interactable kitchen objects

--- CookingSimulator/Assets/BLINDED_AM_ME package/Scripts/ExampleUseof_MeshCut.cs
+++ CookingSimulator/Assets/BLINDED_AM_ME package/Scripts/ExampleUseof_MeshCut.cs
@@ -1,31 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Valve.VR;
 
 public class ExampleUseof_MeshCut : MonoBehaviour {
 
 	public Material capMaterial;
 
-	// Use this for initialization
-	void Start () {
+    public SteamVR_Action_Boolean knifeCut;
 
-		
-	}
+    private bool knifeToggle;
 	
-	void Update(){
-
-		if(Input.GetMouseButtonDown(0)){
+	void Update()
+    {
+        knifeToggle = knifeCut.GetState(SteamVR_Input_Sources.RightHand);
+        if (knifeToggle == true){
 			RaycastHit hit;
 
 			if(Physics.Raycast(transform.position, transform.forward, out hit)){
 
 				GameObject victim = hit.collider.gameObject;
+                if (victim.tag == "Food")
+                {
+                    GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
 
-				GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+                    if (!pieces[1].GetComponent<Rigidbody>())
+                        pieces[1].AddComponent<Rigidbody>();
 
-				if(!pieces[1].GetComponent<Rigidbody>())
-					pieces[1].AddComponent<Rigidbody>();
-
-				Destroy(pieces[1], 1);
+                    Destroy(pieces[1], 1);
+                }
 			}
 
 		}
@@ -35,12 +37,12 @@ public class ExampleUseof_MeshCut : MonoBehaviour {
 
 		Gizmos.color = Color.green;
 
-		Gizmos.DrawLine(transform.position, transform.position + transform.forward * 5.0f);
-		Gizmos.DrawLine(transform.position + transform.up * 0.5f, transform.position + transform.up * 0.5f + transform.forward * 5.0f);
-		Gizmos.DrawLine(transform.position + -transform.up * 0.5f, transform.position + -transform.up * 0.5f + transform.forward * 5.0f);
+		Gizmos.DrawLine(transform.position, transform.position + transform.forward * .15f);
+		Gizmos.DrawLine(transform.position + transform.up * 0.02f, transform.position + transform.up * 0.02f + transform.forward * .15f);
+		Gizmos.DrawLine(transform.position + -transform.up * 0.02f, transform.position + -transform.up * 0.02f + transform.forward * .15f);
 
-		Gizmos.DrawLine(transform.position, transform.position + transform.up * 0.5f);
-		Gizmos.DrawLine(transform.position,  transform.position + -transform.up * 0.5f);
+		Gizmos.DrawLine(transform.position, transform.position + transform.up * 0.02f);
+		Gizmos.DrawLine(transform.position,  transform.position + -transform.up * 0.02f);
 
 	}
 
