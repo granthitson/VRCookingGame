fc0ac4d7ccf52f1104697d746a47a3b96b762636
Grant Hitson <gkh9113@uncw.edu>
2019-10-16 23:43:18 -0400

    stove/oven improvements
    added flames to stove burners. added heat value to cookware - heat value decreasese when not heated, increases when heat is applied.

---
+++ CookingSimulator/Assets/Scripts/Cookware.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cookware : MonoBehaviour
+{
+    [SerializeField]
+    private float heatValue = 0;
+
+    private float maxHeat = 1;
+    private float minHeat = 0;
+
+    private HeatingElement heatingElement;
+
+    private bool heating;
+
+    private void Update()
+    {
+        if (heating == false)
+            heatValue -= 0.0000001f;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        heatingElement = other.gameObject.GetComponent<HeatingElement>();
+
+        if (heatingElement != null)
+        {
+            if (heatingElement.isTurnedOn() == true)
+            {
+                heating = true;
+                heatValue += heatingElement.GetHeatAmount() * .0000001f;
+                heatValue = Mathf.Clamp(heatValue, minHeat, maxHeat);
+            }
+            else 
+            {
+                heating = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        heating = false;
+    }
+}
