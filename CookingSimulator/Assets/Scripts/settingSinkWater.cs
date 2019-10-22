ee055f2c96cbe82b170ffcbafc278bc856d06b44
laurenr14 <55765192+laurenr14@users.noreply.github.com>
2019-10-22 12:24:25 -0400

    Add files via upload

---
+++ CookingSimulator/Assets/Scripts/settingSinkWater.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class settingSinkWater : MonoBehaviour
+{
+
+    public Transform player;
+    public CircularDrive waterKnob;
+    private float knobAngle;
+    public GameObject sinkWater;
+    public Collider waterCollider;
+    public GameObject potWater;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sinkWater.SetActive(false);
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        knobAngle = waterKnob.outAngle;
+        if (knobAngle > 1)
+        {
+            sinkWater.SetActive(true);
+        }
+        if (knobAngle < 1)
+        {
+            sinkWater.SetActive(false);
+        }
+            
+    }
+
+  
+
+
+
+
+}
