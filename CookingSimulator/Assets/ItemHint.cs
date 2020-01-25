8d32fa9c2c292d8ebba3b6e0ffe3151f8448ec54
Grant Hitson <gkh9113@uncw.edu>
2020-01-25 15:14:22 -0500

    misc

--- CookingSimulator/Assets/ItemHint.cs
+++ CookingSimulator/Assets/ItemHint.cs
@@ -14,35 +14,35 @@ public class ItemHint : MonoBehaviour
         
     }
 
-    private void OnHandHoverBegin(Hand hand)
-    {
-        if (knife)
-            SceneHandler.rightHand.ShowGrabHint("Snap Grab");
-        else
-            hand.ShowPinchHint("Grab");
-    }
+    //private void OnHandHoverBegin(Hand hand)
+    //{
+    //    if (knife)
+    //        SceneHandler.rightHand.ShowGrabHint("Snap Grab");
+    //    else
+    //        hand.ShowPinchHint("Grab");
+    //}
 
-    private void OnAttachedToHand(Hand hand)
-    {
-        if (remote)
-            SceneHandler.rightHand.ShowInteractAction("Interact with UI");
+    //private void OnAttachedToHand(Hand hand)
+    //{
+    //    if (remote)
+    //        SceneHandler.rightHand.ShowInteractAction("Interact with UI");
 
-        if (knife)
-            SceneHandler.rightHand.ShowCutAction("Cut");
+    //    if (knife)
+    //        SceneHandler.rightHand.ShowCutAction("Cut");
 
-        if (soap)
-            SceneHandler.rightHand.ShowInteractAction("Soap");
-    }
+    //    if (soap)
+    //        SceneHandler.rightHand.ShowInteractAction("Soap");
+    //}
 
-    private void OnDetachedFromHand(Hand hand)
-    {
-        ControllerButtonHints.HideAllTextHints(SceneHandler.leftHand);
-        ControllerButtonHints.HideAllButtonHints(SceneHandler.leftHand);
+    //private void OnDetachedFromHand(Hand hand)
+    //{
+    //    ControllerButtonHints.HideAllTextHints(SceneHandler.leftHand);
+    //    ControllerButtonHints.HideAllButtonHints(SceneHandler.leftHand);
 
-        ControllerButtonHints.HideAllTextHints(SceneHandler.rightHand);
-        ControllerButtonHints.HideAllButtonHints(SceneHandler.rightHand);
+    //    ControllerButtonHints.HideAllTextHints(SceneHandler.rightHand);
+    //    ControllerButtonHints.HideAllButtonHints(SceneHandler.rightHand);
 
-        ControllerButtonHints.HideAllTextHints(hand);
-        ControllerButtonHints.HideAllButtonHints(hand);
-    }
+    //    ControllerButtonHints.HideAllTextHints(hand);
+    //    ControllerButtonHints.HideAllButtonHints(hand);
+    //}
 }
